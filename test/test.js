a604e91e589b07542338e9804dc4975e0cbb15af
Trygve Lie <post@trygve-lie.com>
2016-11-21 15:21:28 +0100

    initial test setup

---
+++ test/test.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const tap = require('tap');
+const Writer = require('../');
+
+tap.test('foo() - bar', function (t) {
+//    var writer = new Writer('./test/mock/main.js');
+//    writer.bundle().pipe(process.stdout);
+
+    t.equal(true, true);
+    t.end();
+});
