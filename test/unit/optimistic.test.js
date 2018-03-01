53355c6fc3dbc6b55cf9ad830505e71febcdca9d
Richard Walker <digitalsadhu@gmail.com>
2018-03-01 13:28:18 +0100

    refactor: update interface to OptimisticBundler

--- test/unit/optimistic.test.js
+++ test/unit/optimistic.test.js
@@ -36,13 +36,13 @@ const feed3 = [
 
 test('bundleExists() - doesnt exists', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     expect(await ob.bundleExists(['a', 'b', 'c'], 'js')).toBeFalsy();
 });
 
 test('bundleExists() - exists', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     const file =
         'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855.js';
     sink.db[file] = 'content';
@@ -51,7 +51,7 @@ test('bundleExists() - exists', async () => {
 
 test('getFeeds()', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     sink.db['a.json'] = '1';
     sink.db['b.json'] = '2';
     sink.db['c.json'] = '3';
@@ -65,7 +65,7 @@ test('getFeeds()', async () => {
 
 test('bundle()', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
 
     sink.db['hash1.json'] = JSON.stringify(feed1);
     sink.db['hash2.json'] = JSON.stringify(feed2);
@@ -82,7 +82,7 @@ test('bundle()', async () => {
 
 test('bundleIfNeeded() - produces new bundle', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     sink.db['hash1.json'] = JSON.stringify(feed1);
     sink.db['hash2.json'] = JSON.stringify(feed2);
     sink.db['hash3.json'] = JSON.stringify(feed3);
@@ -92,12 +92,12 @@ test('bundleIfNeeded() - produces new bundle', async () => {
         'c/js': 'hash3',
     });
     await ob.bundleIfNeeded(['a', 'b', 'c'], 'js');
-    expect(sink).toMatchSnapshot();
+    expect({ sink }).toMatchSnapshot();
 });
 
 test('bundleIfNeeded() - does not need to produce bundle', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     sink.db['a-hash1.json'] = JSON.stringify(feed1);
     sink.db['b-hash2.json'] = JSON.stringify(feed2);
     sink.db['c-hash3.json'] = JSON.stringify(feed3);
@@ -110,17 +110,17 @@ test('bundleIfNeeded() - does not need to produce bundle', async () => {
         'e867367ae217ade7ab1acf25afbb04cf3f3ad88cba5022e3c63a328db2124194.js';
     sink.db[file] = 'content was not replaced';
     await ob.bundleIfNeeded(['a', 'b', 'c'], 'js');
-    expect(sink).toMatchSnapshot();
+    expect({ sink }).toMatchSnapshot();
 });
 
 test('publishAssets() - invalid schema given', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     expect(ob.publishAssets({}, 'js')).rejects.toMatchSnapshot();
 });
 
 test('publishInstructions() - invalid schema given', async () => {
     const sink = new Sink();
-    const ob = new OptimisticBundler(sink);
+    const ob = new OptimisticBundler({ sink });
     expect(ob.publishInstructions({}, 'js')).rejects.toMatchSnapshot();
 });
