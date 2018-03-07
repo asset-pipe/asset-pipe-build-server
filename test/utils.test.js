8f181e61fbc372be8550ef48da9c8cdb3fefe681
Richard Walker <digitalsadhu@gmail.com>
2018-03-07 11:12:23 +0100

    refactor: split up options to optimistic bundler
    Options can now be passed at instantiation and overridden when calling
    specific methods.

--- test/utils.test.js
+++ test/utils.test.js
@@ -75,3 +75,18 @@ test('passing options to bundleFeeds()', async () => {
     expect(result).toMatchSnapshot();
     endWorkers();
 });
+
+test('booleanWithDefault', () => {
+    expect.hasAssertions();
+    const { booleanWithDefault, endWorkers } = require('../lib/utils');
+    expect(booleanWithDefault('asdasd', 'fallback')).toBe('fallback');
+    expect(booleanWithDefault(null, 'fallback')).toBe('fallback');
+    expect(booleanWithDefault(undefined, 'fallback')).toBe('fallback');
+    expect(booleanWithDefault(1, 'fallback')).toBe('fallback');
+    expect(booleanWithDefault({}, 'fallback')).toBe('fallback');
+    expect(booleanWithDefault('true', 'fallback')).toBe(true);
+    expect(booleanWithDefault('false', 'fallback')).toBe(false);
+    expect(booleanWithDefault(true, 'fallback')).toBe(true);
+    expect(booleanWithDefault(false, 'fallback')).toBe(false);
+    endWorkers();
+});
